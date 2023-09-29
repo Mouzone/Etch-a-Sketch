662c7b38cf8600169242b9aebd3aafdd6653c714
Mouzone <sunnyliu010@gmail.com>
2023-09-28 20:38:45 -0700

    Created createdColumn function

--- script.js
+++ script.js
@@ -7,11 +7,28 @@ function findLength(num_rows){
     return length
 }
 
-function createRows(length){
+function createRows(length, num_rows){
+
     let row = document.createElement('div')
     row.style.width = length;
     row.style.height = width;
+    row.style.border = "2px solid"
+
     for (let i= 0; i<num_rows; i++){
+        row = createColumns(row, length)
         drawing_area.appendChild(row)
     }
 }
+
+function createColumns(row, length, num_rows){
+
+    let column_square = document.createElement('div')
+    column_square.style.width = length;
+    column_square.style.height = length;
+    column_square.style.border = "2px solid"
+
+    for (let i= 0; i<num_rows; i++){
+        row.appendChild(column_square)
+    }
+}
+
