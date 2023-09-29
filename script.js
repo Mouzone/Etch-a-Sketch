12059a9d93bbc7a4e9dbec6232dfeeef980fbfaa
Mouzone <sunnyliu010@gmail.com>
2023-09-28 20:47:18 -0700

    Moved scrip0t tag to the end and created ten boxes

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 const height = 600.0;
 const width = 600.0;
-const drawing_area = document.querySelector(div.drawing-area)
+const drawing_area = document.querySelector('.drawing-area');
 
-function findLength(num_rows){
-    length = 600/num_rows
-    return length
+function findLength(num_rows) {
+    const length = width / num_rows;
+    return length;
 }
 
-function createRows(length, num_rows){
+function createRows(length, num_rows) {
+    const row = document.createElement('div');
+    row.style.width = width + 'px';
+    row.style.height = length + 'px';
+    row.style.border = '2px solid';
 
-    let row = document.createElement('div')
-    row.style.width = length;
-    row.style.height = width;
-    row.style.border = "2px solid"
+    createColumns(row, length, num_rows);
+}
 
-    for (let i= 0; i<num_rows; i++){
-        row = createColumns(row, length)
-        drawing_area.appendChild(row)
+function createColumns(row, length, num_rows) {
+    for (let i = 0; i < num_rows; i++) {
+        const column_square = document.createElement('div');
+        column_square.style.width = length + 'px';
+        column_square.style.height = length + 'px';
+        column_square.style.border = '2px solid';
+
+        row.appendChild(column_square);
+        drawing_area.appendChild(row);
     }
 }
 
-function createColumns(row, length, num_rows){
-
-    let column_square = document.createElement('div')
-    column_square.style.width = length;
-    column_square.style.height = length;
-    column_square.style.border = "2px solid"
-
-    for (let i= 0; i<num_rows; i++){
-        row.appendChild(column_square)
-    }
-}
-
+createRows(findLength(10), 10);
