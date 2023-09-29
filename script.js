0455f110e2139e513a1092176d2442b08f9f3155
Mouzone <sunnyliu010@gmail.com>
2023-09-28 21:18:15 -0700

    Able to spawn grid with appropriate squares
    The width of squares spwans too much, might be arithmetic error

--- script.js
+++ script.js
@@ -26,11 +26,35 @@ function createColumns(row, length, num_rows) {
         column_square.style.width = length + 'px';
         column_square.style.height = length + 'px';
         column_square.style.border= '1px solid';
-
+        column_square.addEventListener('mouseenter', function () {
+            column_square.style.backgroundColor = 'black';
+        });
         row.appendChild(column_square);
     }
     drawing_area.appendChild(row);
 
 }
 
-createRows(findLength(10), 10);
+window.onload = function () {
+    let num_rows;
+    
+    do {
+        // Display a prompt and store the user's input as a string
+        const userInput = window.prompt('Please enter an integer between 0 and 100:');
+        
+        // Convert the user input to an integer
+        num_rows = parseInt(userInput, 10);
+
+        // Check if the user entered a valid integer within the range
+        if (!isNaN(num_rows) && Number.isInteger(num_rows) && num_rows >= 0 && num_rows <= 100) {
+            alert(`You entered the integer: ${num_rows}`);
+
+        } else {
+            alert('Invalid input. Please enter a valid integer between 0 and 100.');
+        }
+    } while (isNaN(num_rows) || !Number.isInteger(num_rows) || num_rows < 0 || num_rows > 100);
+
+    createRows(findLength(num_rows), num_rows)
+};
+
+
