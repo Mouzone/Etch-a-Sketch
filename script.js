140f8b882fcde3078af52a5bd0d809bdec029e46
Mouzone <sunnyliu010@gmail.com>
2023-09-28 20:33:48 -0700

    Created findLength and createRows function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello World')
+const height = 600.0;
+const width = 600.0;
+const drawing_area = document.querySelector(div.drawing-area)
+
+function findLength(num_rows){
+    length = 600/num_rows
+    return length
+}
+
+function createRows(length){
+    let row = document.createElement('div')
+    row.style.width = length;
+    row.style.height = width;
+    for (let i= 0; i<num_rows; i++){
+        drawing_area.appendChild(row)
+    }
+}
