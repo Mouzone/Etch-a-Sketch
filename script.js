9ce6d1e110f10cb834203a7b7fa83b3f17e6b63e
Mouzone <sunnyliu010@gmail.com>
2023-09-29 18:38:57 -0700

    Merged createColumns adn createRows into createDrawingArea

--- script.js
+++ script.js
@@ -1,50 +1,36 @@
-
 const drawing_area = document.querySelector('.drawing-area');
 
-function createRows(num_rows) {
-    for (let i = 0; i < num_rows; i++){
-        const row = document.createElement('div');
-        row.style.border = '.5px solid';
-        row.style.display = "flex"
-        row.style.flexGrow = "1"
-        createColumns(row, num_rows);
-    }
-}
-
-function createColumns(row, num_rows) {
-
+function createDrawingArea(num_rows) {
     for (let i = 0; i < num_rows; i++) {
-        const column_square = document.createElement('div');
-        column_square.style.flexGrow = "1"
-        column_square.style.border= '.5px solid';
-        column_square.addEventListener('mouseenter', function () {
-            column_square.style.backgroundColor = 'black';
-        });
-        row.appendChild(column_square);
+        const row = document.createElement('div');
+        row.style.cssText = 'display: flex; flex-grow: 1; border: 0.5px solid;';
+        
+        for (let j = 0; j < num_rows; j++) {
+            const column_square = document.createElement('div');
+            column_square.style.cssText = 'flex-grow: 1; border: 0.5px solid;';
+            column_square.addEventListener('mouseenter', () => {
+                column_square.style.backgroundColor = 'black';
+            });
+            row.appendChild(column_square);
+        }
+        
+        drawing_area.appendChild(row);
     }
-    drawing_area.appendChild(row);
 }
 
 window.onload = function () {
     let num_rows;
     
     do {
-        // Display a prompt and store the user's input as a string
         const userInput = window.prompt('Please enter an integer between 0 and 100:');
-        
-        // Convert the user input to an integer
         num_rows = parseInt(userInput, 10);
 
-        // Check if the user entered a valid integer within the range
         if (!isNaN(num_rows) && Number.isInteger(num_rows) && num_rows >= 0 && num_rows <= 100) {
             alert(`You entered the integer: ${num_rows}`);
-
         } else {
             alert('Invalid input. Please enter a valid integer between 0 and 100.');
         }
     } while (isNaN(num_rows) || !Number.isInteger(num_rows) || num_rows < 0 || num_rows > 100);
 
-    createRows(num_rows)
+    createDrawingArea(num_rows);
 };
-
-
